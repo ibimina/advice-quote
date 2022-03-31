8e4cabe4187cd3bc736c9789cdb03a14500b8065
ibimina <96787210+ibimina@users.noreply.github.com>
2022-03-31 12:52:31 +0100

    advice quote generation

---
+++ script.js
@@ -0,0 +1,28 @@
+const main = document.querySelector('main')
+const title = document.querySelector('h4')
+const text = document.querySelector('blockquote')
+const dice = document.querySelector('.dice')
+
+const url = "https://api.adviceslip.com/advice"
+const randomAdvice = async ()=>{
+
+    try {
+        let response = await fetch(url)
+        let advice = await response.json()
+          title.textContent = 'ADVICE #'+ advice.slip.id
+        text.textContent = `"${advice.slip.advice}"`
+        
+       
+    } catch (error) {
+        console.log('p')
+    }
+
+}
+window.onload=randomAdvice();
+
+dice.addEventListener('click', e =>{
+    //console.log('p')
+    e.preventDefault()
+    randomAdvice()
+
+})
