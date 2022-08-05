243c941c29000ea8493d62eb4937700dc1acc699
ibimina <96787210+ibimina@users.noreply.github.com>
2022-08-05 20:53:43 +0100

    refactoring old code

--- script.js
+++ script.js
@@ -1,29 +1,28 @@
-const main = document.querySelector('main')
+// const main = document.querySelector('main')
 const title = document.querySelector('h4')
 const text = document.querySelector('blockquote')
 const dice = document.querySelector('.dice')
 
 const url = "https://api.adviceslip.com/advice"
+console.log(url)
 const randomAdvice = async ()=>{
-
     try {
         let response = await fetch(url)
         let advice = await response.json()
+        console.log(advice)
           title.textContent = 'ADVICE #'+ advice.slip.id
-        text.textContent = `"${advice.slip.advice}"`
-        
+        text.textContent = `"${advice.slip.advice}"`  
        
     } catch (error) {
-       // console.log('p')
        text.textContent = `oops something went wrong. Kindly refresh page`
     }
-
 }
-randomAdvice();
+randomAdvice()
+dice.addEventListener("click",randomAdvice)
 
-dice.addEventListener('click', e =>{
-    //console.log('p')
-    e.preventDefault()
-    randomAdvice()
+// dice.addEventListener('click', e =>{
+//     //console.log('p')
+//     e.preventDefault()
+//     randomAdvice()
 
-})
+// })
